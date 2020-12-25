444e8597838b42d135f28bbcfa8a6bd2079c1ba6
ledinhtuyenbkdn <ledinhtuyenbkdn@gmail.com>
2020-12-25 22:44:03 +0700

    fix(chapter-4): regex

--- src/app/Chapter4Regex.java
+++ src/app/Chapter4Regex.java
@@ -8,6 +8,9 @@ public class Chapter4Regex {
         Chapter4Regex chapter4Regex = new Chapter4Regex();
         chapter4Regex.testForPattern();
         chapter4Regex.findMatchingText();
+        chapter4Regex.replaceMatchingText();
+        chapter4Regex.replaceMatchingTextByMulti();
+        chapter4Regex.replaceMatchingTextByInterchange();
     }
 
     private void testForPattern() {
@@ -61,5 +64,57 @@ public class Chapter4Regex {
         if (matcher1.find()) {
             System.out.println(matcher1.group(2) + " " + matcher1.group(1));
         }
+    }
+
+    private void replaceMatchingText() {
+        String inputString = "I love you 3000 and 4000.";
+        String stringRegexPattern = "\\d+";
+
+        Pattern pattern = Pattern.compile(stringRegexPattern);
+        Matcher matcher = pattern.matcher(inputString);
+
+        String result = matcher.replaceAll("replaced");
+        System.out.println(result);
+
+//        StringBuilder stringBuilder = new StringBuilder("abc ");
+//        while (matcher.find()) {
+//            matcher.appendReplacement(stringBuilder, "replaced");
+//        }
+//        matcher.appendTail(stringBuilder);
+//        System.out.println(stringBuilder);
+    }
+
+    private void replaceMatchingTextByMulti() {
+        String inputString = "I love cat and dog";
+        String stringRegexPattern = "cat|dog";
+
+        Pattern pattern = Pattern.compile(stringRegexPattern);
+        Matcher matcher = pattern.matcher(inputString);
+
+        while (matcher.find()) {
+            String found = matcher.group(); // matcher.group(0)
+            String replacement = computeReplacement(found);
+            stringRegexPattern = matcher.replaceFirst(replacement);
+            matcher.reset(stringRegexPattern);
+        }
+        System.out.println(stringRegexPattern);
+    }
+
+    private String computeReplacement(String in) {
+        switch(in) {
+            case "cat": return "feline";
+            case "dog": return "canine";
+            default: return "animal";
+        }
+    }
+
+    private void replaceMatchingTextByInterchange() {
+        String inputString = "Le Tuyen";
+        String stringRegexPattern = "(\\w+)\\s+(\\w+)";
+
+        Pattern pattern = Pattern.compile(stringRegexPattern);
+        Matcher matcher = pattern.matcher(inputString);
+
+        System.out.println(matcher.replaceAll("$2 $1"));
     }
 }
